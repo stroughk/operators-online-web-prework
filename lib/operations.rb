d5e748e508c6c375505378b417a0d19ee5297658
Kateryna Strough <katerynastrough@hotmail.com>
2019-04-20 16:45:21 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,18 @@
 def unsafe?(speed)
-
+speed = 50
+if speed < 40
+  "unsafe"
+else
+  "safe"
+end
 end
 
 
 
 def not_safe?(speed)
-	
+	speed = 50
+	speed < 50 ? "not safe" : "safe" 
+	end
 end
 	
 
